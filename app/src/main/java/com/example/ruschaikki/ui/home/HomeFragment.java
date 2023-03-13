58bff33de62752b291f405bf6f90a58c2c4a58e1
Asadbek Axmedov <100250308+asadbekakmedov2001@users.noreply.github.com>
2023-03-13 18:25:35 +0500

    A

--- app/src/main/java/com/example/ruschaikki/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ruschaikki/ui/home/HomeFragment.java
@@ -50,23 +50,23 @@ public class HomeFragment extends Fragment {
 
     private void initiali() {
 
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 1. ЭПОХА ПРОСВЕЩЕНИЯ","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 2. АНГЛИЙСКОЕ ПРОСВЕЩЕНИЕ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 3. ДАНИЕЛЬ ДЕФО.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 4. ДЖОНАТАНА СВИФТ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 5. ПЕРИОД ЗРЕЛОГО ПРОСВЕЩЕНИЯ В АНГЛИИ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 6. СЕНТИМЕНТАЛИЗМ. ТВОРЧЕСТВО СТЕРНА.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 7. ФРАНЦУЗСКАЯ ЛИТЕРАТУРА XVIII ВЕКА.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 8. ФРАНЦУЗСКИЙ РОМАН ПЕРВОЙ ПОЛОВИНЫ XVIII В.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 9. ПЬЕР ОГЮСТЕН КАРОН БОМАРШЕ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 10. ФРАНЦУЗСКАЯ ФИЛОСОФСКАЯ ПРОЗА. ЭНЦИКЛОПЕДИСТЫ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 11. ТВОРЧЕСТВО ЖАН-ЖАКА РУССО. РУССОИЗМ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 12. ИТАЛЬЯНСКАЯ ЛИТЕРАТУРА XVIII ВЕКА.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 13. НЕМЕЦКАЯ ЛИТЕРАТУРА ПРОСВЕЩЕНИЯ. ЛЕССИНГ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 14. ДВИЖЕНИЕ «БУРЯ И НАТИСК». ШИЛЛЕР.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 15. НЕМЕЦКАЯ ЛИТЕРАТУРА ПРОСВЕЩЕНИЯ. ИОГАНН ВОЛЬФГАНГ ГЁТЕ","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тесты","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
-        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Глоссарий","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/shoxumarzoda/ETNOLOGIYA/raw/master/muqqadima.pdf"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 1. ЭПОХА ПРОСВЕЩЕНИЯ","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema1.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 2. АНГЛИЙСКОЕ ПРОСВЕЩЕНИЕ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema2.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 3. ДАНИЕЛЬ ДЕФО.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema3.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 4. ДЖОНАТАНА СВИФТ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema4.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 5. ПЕРИОД ЗРЕЛОГО ПРОСВЕЩЕНИЯ В АНГЛИИ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema5.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 6. СЕНТИМЕНТАЛИЗМ. ТВОРЧЕСТВО СТЕРНА.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema6.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 7. ФРАНЦУЗСКАЯ ЛИТЕРАТУРА XVIII ВЕКА.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema7.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 8. ФРАНЦУЗСКИЙ РОМАН ПЕРВОЙ ПОЛОВИНЫ XVIII В.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema8.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 9. ПЬЕР ОГЮСТЕН КАРОН БОМАРШЕ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema9.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 10. ФРАНЦУЗСКАЯ ФИЛОСОФСКАЯ ПРОЗА. ЭНЦИКЛОПЕДИСТЫ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema10.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 11. ТВОРЧЕСТВО ЖАН-ЖАКА РУССО. РУССОИЗМ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema11.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 12. ИТАЛЬЯНСКАЯ ЛИТЕРАТУРА XVIII ВЕКА.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema12.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 13. НЕМЕЦКАЯ ЛИТЕРАТУРА ПРОСВЕЩЕНИЯ. ЛЕССИНГ.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema13.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 14. ДВИЖЕНИЕ «БУРЯ И НАТИСК». ШИЛЛЕР.","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema14.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тема 15. НЕМЕЦКАЯ ЛИТЕРАТУРА ПРОСВЕЩЕНИЯ. ИОГАНН ВОЛЬФГАНГ ГЁТЕ","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/tema15.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Тесты","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/test.docx"));
+        modelViewPagerList.add(new ModelViewPager(R.drawable.a10,"Глоссарий","ИСТОРИЯ ЗАРУБЕЖНОЙ ЛИТЕРАТУРЫ XVIII ВЕКА","https://github.com/asadbekakmedov2001/RuschaIkki/raw/main/app/src/main/assets/glossary.docx"));
 
 
 
